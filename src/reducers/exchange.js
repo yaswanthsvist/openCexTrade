116c80c66e329b11eec7da239105d3e8a3115e06
yaswanthsvist@gmail.com <yaswanth.m@scarletwireless.com>
2017-07-08 14:39:43 +0530

    TDD exchange reducer is done

--- src/reducers/exchange.js
+++ src/reducers/exchange.js
@@ -1,26 +1,23 @@
 const defExchangeState={
   symbol1:"BTC",
   symbol2:"USD",
-  timeType:"data1m",
+  timeType:"data1h",
   fromTime:"",
   toTime:"",
 }
 const exchange=(state=defExchangeState,action)=>{
   switch (action.type) {
-    case "SET_SYMBOL1":
-      return {...state,symbol1:action.symbol1};
-      break;
-    case "SET_SYMBOL2":
-      return {...state,symbol2:action.symbol2};
+    case "SET_SYMBOLS":
+      return {...state,symbol1:action.symbol1,symbol2:action.symbol2};
       break;
     case "SET_TIME_TYPE":
       return {...state,timeType:action.timeType};
       break;
-    case "SET_FROM_DATE":
-      return {...state,fromDate:action.fromDate};
+    case "SET_FROM_TIME":
+      return {...state,fromTime:action.fromTime};
       break;
-    case "SET_TO_DATE":
-      return {...state,toDate:action.toDate};
+    case "SET_TO_TIME":
+      return {...state,toTime:action.toTime};
       break;
     default:
       return state;
