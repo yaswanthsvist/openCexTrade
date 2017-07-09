1361680ad1fc95f87a424fc9b05cd8f03d683a10
yaswanthsvist@gmail.com <yaswanth.m@scarletwireless.com>
2017-07-09 20:37:11 +0530

    encrypt key,secret with user entered password logic is done.

--- tests/crypto.service.test.js
+++ tests/crypto.service.test.js
@@ -2,9 +2,33 @@ import crypto from '../src/services/crypto';
 import expect from 'expect'
 import deepFreeze from 'deep-freeze';
 describe("crypto",()=>{
+  const message="The truth is a beautiful and terrible thing, and should therefore be treated with great caution  - Dumbledore.";
+  const secret=`delphi is voldmort daughter`;
+  let encrypted='',decrypted='';
   beforeEach(()=>{
 
   })
+  it("encrypt a message",()=>{
+      encrypted = crypto.encryptWithPassword( message , secret );
+      expect(encrypted).toNotEqual(undefined);
+      expect(encrypted).toNotEqual(null);
+      expect(encrypted).toNotEqual('');
+      expect(encrypted).toNotEqual(message);
+      expect(encrypted).toNotEqual(secret);
+  })
+  it("decrypt a message",()=>{
+      decrypted = crypto.decryptWithPassword( encrypted , secret );
+      expect(decrypted).toNotEqual(undefined);
+      expect(decrypted).toNotEqual(null);
+      expect(decrypted).toNotEqual('');
+      expect(decrypted).toNotEqual(encrypted);
+      expect(decrypted).toNotEqual(secret);
+  });
+  it("validate encryptWithPassword & decryptWithPassword",()=>{
+      const encry = crypto.encryptWithPassword( message , secret );
+      const decry = crypto.decryptWithPassword( encry , secret );
+      expect(decry).toEqual(message);
+  });
   it("SIGNATURE SHOULD be HEX DECIMAL & UPPER CASE",()=>{
     const signature=crypto.generateSignature();
     expect(signature).toMatch(/^[A-Z\d]+$/);
