56a101ac62b6ccf35d75f6f1563128bccdf53283
yaswanthsvist@gmail.com <yaswanth.m@scarletwireless.com>
2017-07-11 03:54:38 +0530

    added thunk middle ware.

--- src/actions/exchange.js
+++ src/actions/exchange.js
@@ -1,4 +1,3 @@
-import webService from './../services/webService'
 export const setTimeType=(timeType)=>{
   return {
     type:"SET_TIME_TYPE",
@@ -24,38 +23,4 @@ export const setToTime=(toTime)=>{
     type:"SET_TO_TIME",
     toTime,
   }
-}
-
-/*
-  fetchohlcv is a thunk
-
-*/
-export const fetchohlcv=(exchange,dispatch,time=(new Date()).getTime())=>{
-  const yyyymmdd=getPrevYYYYMMDD(time);
-  const {symbol1,symbol2}=exchange;
-  const uri=`ohlcv/hd/${yyyymmdd}/${symbol1}/${symbol2}`;
-  return (dispatch)=>
-    webService.get(uri).then(
-      response=>{
-        if(response==null){
-
-        }else {
-          console.log(response.time);
-        }
-      },
-      error=>{
-
-      }
-    );
-}
-
-const getPrevYYYYMMDD=(time=86400000)=>{
-  const d=new Date(time-86400000);
-  let mm = d.getMonth() + 1; // getMonth() is zero-based
-  let dd = d.getDate();
-
-  return [d.getFullYear(),
-          (mm>9 ? '' : '0') + mm,
-          (dd>9 ? '' : '0') + dd
-         ].join('');
 }
