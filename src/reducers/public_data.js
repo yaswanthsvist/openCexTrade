4811e47d516030241f00126c7226afbf4057ae16
yaswanthsvist@gmail.com <yaswanth.m@scarletwireless.com>
2017-07-15 11:49:31 +0530

    updated reducers for bitfinex candle events.  Please enter the commit message for your changes. Lines starting

--- src/reducers/public_data.js
+++ src/reducers/public_data.js
@@ -37,31 +37,61 @@ const defBitfinexState={
     chanId:null,
  }
 }
-export const bitfinex=(state=defBitfinexState,action)=>{
+const candles=(state=defBitfinexState.candles,action)=>{
   switch (action.type) {
     case "BITFINEX_SUBSCRIBED_CANDLE":
       return {
-        ...state,
-        candles:{
           key:action.key,
-          data:{},
+          data:[],
           chanId:action.chanId,
-        }
       };
       break;
     case "BITFINEX_UNSUBSCRIBED_CANDLE":
       return {
-        ...state,
-        candles:{
-          ...state.candles,
-          data:{},
+          ...state,
+          data:[],
           chanId:null,
-        }
       };
       break;
+    case "BITFINEX_INITIALIZE_CANDLE":
+      return {
+          ...state,
+          chanId:action.chanId,
+          data:[...action.data],
+      }
+    case "BITFINEX_UPDATE_CANDLE":
+         if(state.chanId!=action.chanId||state.data[0][0]>action.data[0][0]){
+           return state;
+         }
+         if(state.data[0][0]===action.data[0][0]){
+          return {
+              ...state,
+              data:[...action.data,...state.data.slice(1)],
+          }
+        }else if(state.data[0][0]<action.data[0][0]){
+          return {
+              ...state,
+              data:[...action.data,...state.data],
+          }
+        }
+    default:
+    return state;
+  }
+}
+export const bitfinex=(state=defBitfinexState,action)=>{
+  switch (action.type) {
+    case "BITFINEX_SUBSCRIBED_CANDLE":
+    case "BITFINEX_UNSUBSCRIBED_CANDLE":
+    case "BITFINEX_INITIALIZE_CANDLE":
+    case "BITFINEX_UPDATE_CANDLE":
+        return {
+          ...state,
+          candles:candles(state.candles,action),
+        }
     default:
       return state;
   }
 }
+
 
 export default public_data;
